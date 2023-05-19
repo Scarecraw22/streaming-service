6d9d03bc0525fa3e46b06b72be67dc0a66c41a56
Oskar Wasikowski <o.wasikowski@gmail.com>
2023-05-19 20:55:58 +0200

    First commit

---
+++ streaming-service-web/src/main/java/io/github/scarecraw22/streaming/service/StreamingServiceApp.java
@@ -0,0 +1,12 @@
+package io.github.scarecraw22.streaming.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingServiceApp.class, args);
+    }
+}
